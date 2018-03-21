901ec01492b8f731e15174137c2525e6b5ed6971
JohnDavis <jmdcodejunkie@gmail.com>
2018-03-21 13:35:51 -0700

    stop harvesting haulers....

--- Default/role.hauler.js
+++ Default/role.hauler.js
@@ -7,10 +7,6 @@ module.exports =
         if(creep.carry.energy < creep.carryCapacity) 
         {
             var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) 
-            {
-                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
-            }
         }
         else 
         {
@@ -26,7 +22,7 @@ module.exports =
             });
 
             targets = targets.concat(creep.room.find(FIND_CONSTRUCTION_SITES));
-            
+
             if(targets.length > 0)
             {
                 if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) 
