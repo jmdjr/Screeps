a654ee273d0704d2f885a3e8c7c888ea77baff1f
JohnDavis <jmdcodejunkie@gmail.com>
2018-03-21 14:03:07 -0700

    sdf

--- Default/role.hauler.js
+++ Default/role.hauler.js
@@ -6,7 +6,7 @@ module.exports =
     {
         if(creep.carry.energy < creep.carryCapacity) 
         {
-            var sources = creep.room.find(FIND_DROPPED_ENERGY);
+            var sources = creep.room.find(FIND_DROPPED_RESOURCES);
             if(creep.pickup(sources[0]) == ERR_NOT_IN_RANGE) 
             {
                 creep.moveTo(sources[0], { visualizePathStyle: { stroke: '#ffffff' } });
@@ -24,6 +24,8 @@ module.exports =
                         && structure.energy < structure.energyCapacity;
                 }
             });
+
+
 
             if(targets.length > 0)
             {
