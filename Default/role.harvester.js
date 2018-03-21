73b28d76dc2554c9e8cd245459a87c53fd4f9235
JohnDavis <jmdcodejunkie@gmail.com>
2018-03-21 16:32:38 -0700

    sdf

--- Default/role.harvester.js
+++ Default/role.harvester.js
@@ -3,21 +3,31 @@
 
 var SourcesNotHarvested = function(creep) 
 {
-    var sources = creep.room.find(FIND_SOURCES);
-    var taken = Array.from(Game.creeps, c => c.memory.target);
-    return sources.filter(item => !taken.includes(item));
+    var sources = Array.from(creep.room.find(FIND_SOURCES), c => c.id);
+    var taken = Array.from(_.filter(Game.creeps, c => c.memory.role === 'harvester'), c => c.memory.target);
+    return sources.filter(id => !taken.includes(id));
 }
 
 module.exports = 
 {
     run: function(creep) 
     {
-        var availables = SourcesNotHarvested(creep);
-        creep.memory.target = availables[0];
-        
-        if(creep.harvest(creep.memory.target) == ERR_NOT_IN_RANGE) 
+        if(creep.memory.target == null)
         {
-            creep.moveTo(creep.memory.target, {visualizePathStyle: {stroke: '#ffaa00'}});
+            creep.say('looking for target');
+            var availables = SourcesNotHarvested(creep);
+
+            if(availables.length > 0)
+            {
+                creep.memory.target = availables[0];
+                creep.say(creep.memory.target);
+            }
+        }
+
+        var target = Game.getObjectById(creep.memory.target);
+        if(target != null && creep.harvest(target) == ERR_NOT_IN_RANGE) 
+        {
+            creep.moveTo(target, {visualizePathStyle: {stroke: '#ffaa00'}});
         }
     }
 };
