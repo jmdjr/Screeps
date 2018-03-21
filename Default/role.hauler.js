34378059f136f5a907844e0967fa4c8e8fef4948
JohnDavis <jmdcodejunkie@gmail.com>
2018-03-21 13:45:32 -0700

    haulin

--- Default/role.hauler.js
+++ Default/role.hauler.js
@@ -6,7 +6,11 @@ module.exports =
     {
         if(creep.carry.energy < creep.carryCapacity) 
         {
-            var sources = creep.room.find(FIND_SOURCES);
+            var sources = creep.room.find(FIND_DROPPED_ENERGY);
+            if(creep.pickup(sources[0]) == ERR_NOT_IN_RANGE) 
+            {
+                creep.moveTo(sources[0], { visualizePathStyle: { stroke: '#ffffff' } });
+            }
         }
         else 
         {
