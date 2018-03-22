3a913a0c0c2d90556df8eb3c621d540f2a75f8f3
JohnDavis <jmdcodejunkie@gmail.com>
2018-03-22 14:43:19 -0700

    sdf

--- Default/role.stem.js
+++ Default/role.stem.js
@@ -23,10 +23,7 @@ module.exports =
 
             if(targets.length > 0)
             {
-                if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) 
-                {
-                    creep.moveTo(targets[0], { visualizePathStyle: { stroke: '#ffffff' } });
-                }
+                cUtility.MoveToDo(creep, creep.transfer, targets[0], false);
             }
         }
 
