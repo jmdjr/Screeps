8a7f57ed5e139f371c88fa61a9910ca047938e9a
JohnDavis <jmdcodejunkie@gmail.com>
2018-03-22 14:39:13 -0700

    asd

--- Default/role.harvester.js
+++ Default/role.harvester.js
@@ -3,12 +3,12 @@
 var cUtility = require("creepUtility");
 var CreepType = cUtility.CreepType;
 
-var sources = [];
 var takenSources = [];
 var perSource = 2;
 
 var SourcesNotAtLimit = function(creep) 
 {
+    var sources = _.toArray(creep.room.find(FIND_SOURCES));
     var s = Array.from(sources, c => c.id);
     return s.filter(id => typeof takenSources[id] == 'undefined' || takenSources[id] <= perSource);
 }
@@ -17,28 +17,27 @@ module.exports =
 {
     run: function(creep) 
     {
-        if(sources == []) 
-        {
-            sources = _.toArray(creep.room.find(FIND_SOURCES));
-        }
 
         var availables = SourcesNotAtLimit(creep);
 
         if(availables.length > 0)
         {
-            creep.memory.target = cUtility.FindClosest(creep, availables);
+            availables = Array.from(availables, a => Game.getObjectById(a));
+            var target = cUtility.FindClosest(creep, availables)[0];
 
-            if(typeof takenSources[creep.memory.target] === 'undefined') 
+            if(typeof takenSources[target.id] === 'undefined') 
             {
-                takenSources[creep.memory.target] = 1;
+                takenSources[target.id] = 1;
             }
             else 
             {
-                takenSources[creep.memory.target] += 1;
+                takenSources[target.id] += 1;
             }
+            
+            creep.memory.target = target.id;
         }
 
         var target = Game.getObjectById(creep.memory.target);
-        cUtility.MoveToDo(creep, target, true);
+        cUtility.MoveToDo(creep, creep.harvest, target, true);
     }
 };
