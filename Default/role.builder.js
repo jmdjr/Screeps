47aeea637a498e9c43f06ac66e6269ed198f9602
John M Davis Jr <836561+jmdjr@users.noreply.github.com>
2018-03-22 23:44:30 -0700

    a couple of things

--- Default/role.builder.js
+++ Default/role.builder.js
@@ -6,16 +6,18 @@ var CreepType = cUtility.CreepType;
 module.exports = {
         run: function(creep) 
         {
+            var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
+            var closest = cUtility.FindClosest(creep, targets);
+            
             if(creep.memory.building)
             {
-                var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
                 if(targets.length > 0)
                 {
-                    if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) 
+                    if(creep.build(closest) == ERR_NOT_IN_RANGE) 
                     {
-                        creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                        creep.moveTo(closest, {visualizePathStyle: {stroke: '#ffffff'}});
                     }
-                    else if(creep.build(targets[0]) != OK)
+                    else if(creep.build(closest) != OK)
                     {
                         creep.memory.building = false;
                     }
