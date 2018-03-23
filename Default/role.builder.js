3d3d6b203d496f5e6df5437ec9cccb24ae8958f4
John M Davis Jr <836561+jmdjr@users.noreply.github.com>
2018-03-22 23:04:56 -0700

    lets try building

--- Default/role.builder.js
+++ Default/role.builder.js
@@ -1,44 +1,22 @@
 //------------------------------------------------------------------------------------------------------
 // role.builder
+var cUtility = require("creepUtility");
+var CreepType = cUtility.CreepType;
+
 module.exports = {
-        run: function(creep) {
-            
-            if(!creep.memory.building) {
-                var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
+        run: function(creep) 
+        {
+            var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
 
-                if(targets.length > 0) {
-                    targets.sort((ta, tb) => (ta.progress / ta.progressTotal) - (tb.progress / tb.progressTotal) );
-                    creep.memory.target = targets[0].id;
+            if(targets.length > 0) {
 
-                    target = Game.getObjectById(creep.memory.target);
-
-                    if(creep.build(target) == ERR_NOT_IN_RANGE) {
-                        creep.moveTo(target, {visualizePathStyle: {stroke: '#ffffff'}});
-                    }
-                    else 
-                    {
-                        creep.memory.building = true;
-                    }
+                if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
                 }
-            }
-            else
-            {
-                var target = Game.getObjectById(creep.memory.target.id);
-
-                if(target == null 
-                    || target.progress == target.progressTotal) 
+                else 
                 {
-                    creep.memory.building = false;
-                }
-                else if(creep.build(target) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(target, {visualizePathStyle: {stroke: '#ffffff'}});
+                    creep.memory.building = true;
                 }
             }
-            // else {
-            //     var sources = creep.room.find(FIND_SOURCES);
-            //     if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-            //         creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
-            //     }
-            // }
         }
     };
