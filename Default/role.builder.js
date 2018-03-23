893d151c5cb0ae3eb339471a2850e7f59df4353d
John M Davis Jr <836561+jmdjr@users.noreply.github.com>
2018-03-22 23:19:53 -0700

    builders

--- Default/role.builder.js
+++ Default/role.builder.js
@@ -6,17 +6,16 @@ var CreepType = cUtility.CreepType;
 module.exports = {
         run: function(creep) 
         {
-            var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
-
             if(creep.memory.building)
             {
+                var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
                 if(targets.length > 0)
                 {
                     if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) 
                     {
                         creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
                     }
-                    else 
+                    else if(creep.build(targets[0]) != OK)
                     {
                         creep.memory.building = false;
                     }
