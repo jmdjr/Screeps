6142afd0d6be30c4de4b99dacd3c6199250988b1
JohnDavis <jmdcodejunkie@gmail.com>
2018-03-28 08:23:29 -0700

    harvesting updates

--- Default/role.harvester.js
+++ Default/role.harvester.js
@@ -3,38 +3,68 @@
 var cUtility = require("creepUtility");
 var CreepType = cUtility.CreepType;
 
-var takenSources = [];
+Memory.takenSources = [];
 var perSource = 2;
 
 var SourcesNotAtLimit = function(creep) 
 {
     var sources = _.toArray(creep.room.find(FIND_SOURCES));
     var s = Array.from(sources, c => c.id);
-    return s.filter(id => typeof takenSources[id] == 'undefined' || takenSources[id] <= perSource);
+    return s.filter(id => typeof Memory.takenSources[id] == 'undefined' || Memory.takenSources[id] <= perSource);
+}
+
+var AddTakenSource = function(list, id) 
+{
+    if(typeof list[id] === 'undefined') 
+    {
+        list.push(id);
+        list[id] = 1;
+    }
+    else 
+    {
+        list[id] += 1;
+    }
+
+    return list;
 }
 
 module.exports = 
 {
     run: function(creep) 
     {
+        if(Memory.takenSources == []) 
+        {
+            Memory.takenSources = _.toArray(cUtility.FilterCreeps(CreepType.harvest));
+
+            if(Memory.takenSources.length > 0)
+            {
+                var ids = Memory.takenSources.from((a) => a.memory.target);
+                Memory.takenSources = [];
+
+                for(var id in ids) 
+                {
+                    Memory.takenSources = AddTakenSource(Memory.takenSources, id);
+                }
+            }
+
+            console.log(Memory.takenSources);
+        }
 
         var availables = SourcesNotAtLimit(creep);
 
+        console.log(availables);
+            
         if(availables.length > 0 && creep.memory.target == null)
         {
             availables = Array.from(availables, a => Game.getObjectById(a));
-            var target = cUtility.FindClosest(creep, availables)[0];
+            var targets = cUtility.FindClosest(creep, availables);
+            console.log(Memory.takenSources);
 
-            if(typeof takenSources[target.id] === 'undefined') 
+            if(targets.length > 0) 
             {
-                takenSources[target.id] = 1;
+                creep.memory.target = targets[0].id;
+                AddTakenSource(Memory.takenSources, creep.memory.target)
             }
-            else 
-            {
-                takenSources[target.id] += 1;
-            }
-            
-            creep.memory.target = target.id;
         }
 
         var target = Game.getObjectById(creep.memory.target);
