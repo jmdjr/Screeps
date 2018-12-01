f71b47ec48cf38d92c0f6bb124a1a69cb70f000b
John M Davis Jr <836561+jmdjr@users.noreply.github.com>
2018-12-01 15:55:44 -0800

    Update OverMind.js

--- UniversalConscience/OverMind.js
+++ UniversalConscience/OverMind.js
@@ -8,11 +8,11 @@ module.exports =
         },
         AddAllAvailableRooms:function() {
             for(const i in Game.rooms) {
-                var room = Game.rooms[i];                
+                // var room = Game.rooms[i];                
                 // room.Memory = {
                 //     Objective: "Testing..."
                 // };
-                this.Memory.Rooms.push(room);
+                this.Memory.Rooms.push(Game.rooms[i]);
             }
         },
         Print: function() {
