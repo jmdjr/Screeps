f8b6b7bb768257dfc52a381ec18e926950fb7c74
John M Davis Jr <836561+jmdjr@users.noreply.github.com>
2018-12-01 14:14:35 -0800

    awer

--- UniversalConscience/main.js
+++ UniversalConscience/main.js
@@ -1,6 +1,14 @@
 
 var OM = require('OverMind');
 
+// the overmind has just 
+console.log("test awake");
+
 module.exports.loop = function () {
-    
+    //Overmind needs to look at rooms, and try and work towards completing their objectives.
+    // - look at first room
+    //    - room report current objective (upgrading: needs a constant upgrader with energy flowing to them.)
+    //       - upgrader 
+
+    console.log("tick");
 }
