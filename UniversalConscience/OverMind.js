20804f0584e085bfaccf579a83fa8e23eca8fac3
John M Davis Jr <836561+jmdjr@users.noreply.github.com>
2018-12-01 15:23:14 -0800

    Update OverMind.js

--- UniversalConscience/OverMind.js
+++ UniversalConscience/OverMind.js
@@ -2,23 +2,28 @@
 module.exports =
 {
     OverMind: {
-        IsAwake: false,
-        Rooms: [],
+        Memory: {
+            IsAwake: false,
+            Rooms: [],
+            Sources: []
+        },
         Print: function() {
-            console.log("A Function Exsists");
-        }
+            console.log(this.Memory.IsAwake);
+        } 
     },
+
     // First thing, WakeUp
     InitializeOM: function() {
         if(!Memory.OverMind) {
-            Memory.OverMind = this.OverMind;
+            Memory.OverMind = this.OverMind.Memory;
             console.log("initialize overmind");
             this.OverMind.Print();
+            this.OverMind.Memory.IsAwake = true;
         }
         else
         {
-            this.OverMind = Memory.OverMind;
-            console.log("reload remembered overmind");
+            this.OverMind.Memory = Memory.OverMind;
+            console.log("overmind remembers");
             this.OverMind.Print();
         }
     },
