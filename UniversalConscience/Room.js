3d09d6b4747cdc5afd06d09a4d9b6f5e40293517
John M Davis Jr <836561+jmdjr@users.noreply.github.com>
2018-12-04 15:07:12 -0800

    Update Room.js

--- UniversalConscience/Room.js
+++ UniversalConscience/Room.js
@@ -1,10 +1,11 @@
 // A Room.
 module.exports = {
-    Room: function(data) {
-        console.log("this possibly works");
-    },
 
     RealizeRoom: function(data) {
         return new this.Room(data);
-    }
+    },
+    
+    Room: function(data) {
+        console.log("this possibly works");
+    },
 }
