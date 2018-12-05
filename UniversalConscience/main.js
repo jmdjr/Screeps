093adeb3e3b4e2742378c54efe56fb4d04965868
John M Davis Jr <836561+jmdjr@users.noreply.github.com>
2018-12-05 12:39:37 -0800

    Update main.js

--- UniversalConscience/main.js
+++ UniversalConscience/main.js
@@ -3,8 +3,8 @@ var OMSystem = require('OverMind');
 var primaryObjectives = require('PrimaryObjective');
 
 // the overmind has just 
-var Overmind = new OMSystem(primaryObjectives);
-Overmind.Awaken();
+// var Overmind = new OMSystem(primaryObjectives);
+// Overmind.Awaken();
 
 module.exports.loop = function () {
     // Overmind.Think();
