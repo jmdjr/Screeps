18519dfdfa0c2a2e044fbccc9f5fe18f299c045e
John M Davis Jr <836561+jmdjr@users.noreply.github.com>
2018-12-05 13:02:48 -0800

    Update main.js

--- UniversalConscience/main.js
+++ UniversalConscience/main.js
@@ -6,14 +6,7 @@ var Room = require('Room');
 // the overmind has just 
 // var Overmind = new OMSystem();
 // Overmind.Awaken();
-
-Memory.Test = {
-    name: "Test 1"
-}
-
 Memory.Room = new Room(Game.rooms[0]);
-
-Room.run.call(Memory.Test);
 
 module.exports.loop = function () {
     // Overmind.Think();
